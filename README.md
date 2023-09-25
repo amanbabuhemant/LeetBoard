043ede733f738f5fe606d68736e4135e87ea18fb
Aman Babu Hemant <amanbabuhemant@gmail.com>
2023-09-25 14:37:33 +0530

    code files added

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# LeetBoard
+# LeetBoard - LeetCode Leaderboard
+
+LeetBoard is a Python-based project that allows users to create and maintain a leaderboard for LeetCode profiles. With LeetBoard, you can add, update, and view rankings for LeetCode users. It's a fun and interactive way to track your progress and compete with friends or colleagues on LeetCode.
+
+Visit [LeetBoard - LeetCode Leaderboard](http://leetboard.artizote.com)!
+
+## Features
+
+- Add LeetCode profiles: Anyone can easily add LeetCode usernames to our leaderboard.
+- Update rankings: Anyone can easily update anyone's profile data.
+
+## Getting Started
+
+To run LeetBoard locally or contribute to the project, follow these steps:
+
+1. Clone this repository to your local machine:
+
+   ```shell
+   git clone https://github.com/yourusername/leetboard.git
+   ```
+
+2. Install the required Python packages listed in `requirements.txt` using pip:
+
+   ```shell
+   pip install -r requirements.txt
+   ```
+
+3. Run the LeetBoard application:
+
+   ```shell
+   python leetboard.py
+   ```
+   or
+   ```shell
+   flask run
+   ```
+
+4. Access LeetBoard in your web browser at `http://localhost:5000`.
+
+## Usage
+
+- **Add a Profile:** Add you LeetCode's username and click "Add" button from home [age to fetch and add profile.
+
+- **Update Rankings:** Click on the "Update" button on profile card to fetch and update profile data (fetching only done once a day for a perticular profile).
+
+- **View Rankings:** The leaderboard displays the rankings of added profiles based on LeetCode rank.
+
+## Contributing
+
+Contributions are welcome! If you'd like to contribute to LeetBoard, please follow these steps:
+
+1. Fork the repository and create a new branch for your feature or bug fix.
+2. Make your changes and test thoroughly.
+3. Create a pull request with a description of your changes.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- LeetBoard is not affiliated with LeetCode.
+- Special thanks to the Python community for providing the necessary tools and libraries to build this project.
+
+Happy coding and LeetCoding!
---
+++ app.py
@@ -0,0 +1,107 @@
+from requests import get
+from functools import lru_cache
+import json
+from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
+
+app = Flask("LeetBoard")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    bio = db.Column(db.Text)
+    username = db.Column(db.String, unique=True)
+    dp = db.Column(db.String)
+    rank = db.Column(db.Integer, default=5000000)
+    change = db.Column(db.Integer, default=0)
+    country = db.Column(db.String(16), nullable=True)
+    solutions = db.Column(db.Text, nullable=True)
+    update = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def rank_formated(user):
+        r_rank = str(user.rank)[::-1]
+        f_rank = ""
+        for i in range(len(r_rank)):
+            if i and not i % 3:
+                f_rank = "," + f_rank
+            f_rank = r_rank[i] + f_rank
+        return f_rank
+     
+    def __lt__(user, other):
+        return not user.rank < other.rank
+    
+    def __gt__(user, other):
+        return not user.rank > other.rank
+
+def leetcode_profile(username):
+    url = "https://leetcode.com/" + username
+    page = get(url)
+    if not page.ok:
+        return None
+    page = page.text
+    user = page[page.find("\"profile\":")+10:page.find("\"profile\":") + page[page.find("\"profile\":"):].find("}")+1]
+    page = page[page.find(user)-1000:page.find(user)+1000]
+    #solutions = page[page.find("{\"matchedUser\":")+15:page.find("{\"matchedUser\":") + page[page.find("{\"matchedUser\":"):].find("}")+2]
+    #solutions of the user will be added in soon
+    user = json.loads(user)
+    return user
+
+@app.route("/")
+def _home():
+    users = User.query.order_by(User.rank).all()
+    msg = request.args.get("msg")
+    return render_template("home.html", users=users, msg=msg)
+
+@app.route("/update")
+def _update():
+    id = request.args.get("id")
+    try:
+        id = int(id)
+    except:
+        return redirect("/?msg=invalid user id")
+    user = User.query.filter_by(id=id).first()
+    if not user:
+        return redirect("/")
+    if datetime.utcnow() > datetime.utcnow() - timedelta(days=1):
+        return redirect("/?msg=Profile Already Updatet within a day")
+    fetched = leetcode_profile(user.username)
+    user.change = user.rank - fetched["ranking"]
+    user.rank = fetched["ranking"]
+    user.dp = fetched["userAvatar"]
+    user.name = fetched["realName"]
+    user.bio = fetched["aboutMe"]
+    user.country = fetched["countryName"]
+    user.update = datetime.utcnow()
+    db.session.commit()
+    return redirect("/?msg=Profile updated succesfuly")
+
+@app.route("/add", methods=["POST"])
+def _add():
+    username = request.form.get("username")
+    if not username:
+        return redirect("/")
+    username = username.strip().replace("@", "").lower()
+    if user := User.query.filter_by(username=username).first():
+        return redirect("/update?id=" + str(user.id))
+    fetched = leetcode_profile(username)
+    if not fetched:
+        return redirect("/?msg=User not found")
+    user = User(
+        username = username,
+        rank = fetched["ranking"],
+        dp = fetched["userAvatar"],
+        name = fetched["realName"],
+        bio = fetched["aboutMe"],
+        country = fetched["countryName"],
+    )
+    db.session.add(user)
+    db.session.commit()
+    return redirect("/?msg= " + username + "'s Profile added!!")
+
+if __name__ == "__main__":
+    app.run()
